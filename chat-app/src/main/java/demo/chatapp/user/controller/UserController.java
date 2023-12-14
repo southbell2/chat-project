02c37fac2cf2c337337f4a5e184a57dba14b6243
User <southbell2357@naver.com>
2023-12-15 04:24:26 +0900

    Feat: 로그아웃 기능 추가

--- chat-app/src/main/java/demo/chatapp/user/controller/UserController.java
+++ chat-app/src/main/java/demo/chatapp/user/controller/UserController.java
@@ -1,12 +1,19 @@
 package demo.chatapp.user.controller;
 
+import static demo.chatapp.security.SecurityConstants.REFRESH_HEADER;
+
+import demo.chatapp.security.repository.TokenRepository;
 import demo.chatapp.user.service.UserService;
 import demo.chatapp.user.service.vo.SignUpUserRequest;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserController {
 
     private final UserService userService;
+    private final TokenRepository tokenRepository;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpUserRequest userRequest) {
@@ -25,6 +33,29 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<Void> login() {
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        String refreshToken = getRefTokenFromCookie(request);
+        if (StringUtils.hasText(refreshToken)) {
+            tokenRepository.deleteToken(refreshToken);
+        }
+
+        return ResponseEntity.ok().build();
+    }
+
+    private String getRefTokenFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String refToken = null;
+        if (cookies != null) {
+            refToken = Arrays.stream(cookies)
+                .filter(cookie -> REFRESH_HEADER.equals(cookie.getName()))
+                .map(Cookie::getValue)
+                .findFirst()
+                .orElse(null);
+        }
+        return refToken;
     }
 
 
