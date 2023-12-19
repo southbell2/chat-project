60f7c209612e4ee33cbe246b1b1e1abdc41f62e3
User <southbell2357@naver.com>
2023-12-19 09:33:46 +0900

    Test: Security 테스트 추가

---
+++ chat-app/src/test/java/demo/chatapp/security/SecurityTest.java
@@ -0,0 +1,149 @@
+package demo.chatapp.security;
+
+import static demo.chatapp.security.SecurityConstants.ACCESS_HEADER;
+import static demo.chatapp.security.SecurityConstants.REFRESH_HEADER;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import demo.chatapp.user.repository.UserRepository;
+import demo.chatapp.user.service.UserService;
+import demo.chatapp.user.service.dto.SignUpUserRequest;
+import demo.chatapp.user.service.dto.UserInfoResponse;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = true)
+public class SecurityTest {
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    UserRepository userRepository;
+
+    String testEmail;
+    String testNickname;
+    String testPassword;
+
+    @BeforeEach
+    public void setUp() {
+        SignUpUserRequest userRequest = new SignUpUserRequest();
+        testEmail = "test@test.com";
+        testNickname = "Terry";
+        testPassword = "12345";
+        userRequest.setEmail(testEmail);
+        userRequest.setNickname(testNickname);
+        userRequest.setPassword(testPassword);
+
+        userService.signUp(userRequest);
+    }
+
+    @Test
+    public void 로그인_성공() throws Exception {
+        mockMvc.perform(post("/login")
+                .with(httpBasic(testEmail, testPassword)))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void 로그인_실패() throws Exception {
+        //given
+        String wrongPassword = "1234";
+
+        //when && then
+        mockMvc.perform(post("/login")
+                .with(httpBasic(testEmail, wrongPassword)))
+            .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void 로그인_성공_후_유저정보얻기() throws Exception {
+        //given
+        MockHttpServletResponse loginResponse = mockMvc.perform(post("/login")
+                .with(httpBasic(testEmail, testPassword)))
+            .andExpect(status().isOk())
+            .andReturn().getResponse();
+
+        String accessToken = loginResponse.getHeader(ACCESS_HEADER);
+        Cookie refreshToken = loginResponse.getCookie(REFRESH_HEADER);
+
+        //when
+        MockHttpServletResponse mockHttpServletResponse = mockMvc.perform(get("/userinfo")
+                .header(ACCESS_HEADER, accessToken)
+                .cookie(refreshToken))
+            .andExpect(status().isOk())
+            .andReturn().getResponse();
+
+        UserInfoResponse userInfoResponse = objectMapper.readValue(mockHttpServletResponse.getContentAsString(), UserInfoResponse.class);
+
+        //then
+        assertThat(userInfoResponse.getEmail()).isEqualTo(testEmail);
+        assertThat(userInfoResponse.getNickname()).isEqualTo(testNickname);
+    }
+
+    @Test
+    public void 잘못된_Access_Token_보내기() throws Exception {
+        //given
+        MockHttpServletResponse loginResponse = mockMvc.perform(post("/login")
+                .with(httpBasic(testEmail, testPassword)))
+            .andExpect(status().isOk())
+            .andReturn().getResponse();
+
+        String accessToken = loginResponse.getHeader(ACCESS_HEADER);
+        Cookie refreshToken = loginResponse.getCookie(REFRESH_HEADER);
+        String wrongAccessToken = accessToken + "a";
+
+        //when && then
+        mockMvc.perform(get("/userinfo")
+                .header(ACCESS_HEADER, wrongAccessToken)
+                .cookie(refreshToken))
+            .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void Access_Token_재발급_받기() throws Exception {
+        //given
+        MockHttpServletResponse loginResponse = mockMvc.perform(post("/login")
+                .with(httpBasic(testEmail, testPassword)))
+            .andExpect(status().isOk())
+            .andReturn().getResponse();
+
+        String accessToken = loginResponse.getHeader(ACCESS_HEADER);
+        Cookie refreshToken = loginResponse.getCookie(REFRESH_HEADER);
+
+        //when
+        String reIssueAccessToken = mockMvc.perform(get("/refresh-token")
+                .cookie(refreshToken))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getHeader(ACCESS_HEADER);
+
+        String responseBody = mockMvc.perform(get("/userinfo")
+                .header(ACCESS_HEADER, reIssueAccessToken))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+
+        UserInfoResponse userInfoResponse = objectMapper.readValue(responseBody, UserInfoResponse.class);
+
+        //then
+        assertThat(userInfoResponse.getEmail()).isEqualTo(testEmail);
+        assertThat(userInfoResponse.getNickname()).isEqualTo(testNickname);
+    }
+}
