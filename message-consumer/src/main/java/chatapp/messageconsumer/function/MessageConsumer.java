69d25f6ca5e5fbaf8bcfa6a25061d6f6618965cc
User <southbell2357@naver.com>
2024-04-17 20:57:25 +0900

    Feat: message-consumer의 카산드라, 레디스 사용을 비동기 처리로 바꿈

--- message-consumer/src/main/java/chatapp/messageconsumer/function/MessageConsumer.java
+++ message-consumer/src/main/java/chatapp/messageconsumer/function/MessageConsumer.java
@@ -10,6 +10,10 @@ import chatapp.messageconsumer.message.MessageRepository;
 import chatapp.messageconsumer.message.casssandra.Message;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -20,19 +24,25 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Slf4j
 public class MessageConsumer {
 
+    //카산드라에 저장과 레디스에 메시지를 발행하는 작업을 비동기적으로 하기 위한 쓰레드풀
+    private final ExecutorService executorService = new ThreadPoolExecutor(10, 200, 60,
+        TimeUnit.SECONDS, new LinkedBlockingQueue<>(20));
+
     @Bean
     public Consumer<ChatMessage> consume(MessageRepository messageRepository,
         StringRedisTemplate redisTemplate, ObjectMapper objectMapper) {
         return chatMessage -> {
             String subChannel = REDIS_CHANNEL_PREFIX + chatMessage.getChannelId();
-            try {
-                String messageJson = objectMapper.writeValueAsString(chatMessage);
-                redisTemplate.convertAndSend(subChannel, messageJson);
-            } catch (JsonProcessingException e) {
-                log.info("JsonProcessingException = ", e);
-            }
-            Message message = createMessage(chatMessage);
-            messageRepository.save(message);
+            executorService.submit(() -> {
+                try {
+                    String messageJson = objectMapper.writeValueAsString(chatMessage);
+                    redisTemplate.convertAndSend(subChannel, messageJson);
+                } catch (JsonProcessingException e) {
+                    log.info("JsonProcessingException = ", e);
+                }
+                Message message = createMessage(chatMessage);
+                messageRepository.save(message);
+            });
         };
     }
 
