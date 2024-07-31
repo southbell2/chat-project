226e8308ef70b681a00e6e0d2dc455b871aaace1
User <southbell2357@naver.com>
2024-07-31 10:23:08 +0900

    Refactor: 예외 메서드 변경

--- chat-app/src/main/java/demo/chatapp/exception/ErrorController.java
+++ chat-app/src/main/java/demo/chatapp/exception/ErrorController.java
@@ -11,12 +11,12 @@ public class ErrorController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BadRequestException.class)
     public ErrorMessage handleBadRequestError(BadRequestException e) {
-        return new ErrorMessage(e.getLocalizedMessage(), e.getClass().getSimpleName());
+        return new ErrorMessage(e.getMessage(), e.getClass().getSimpleName());
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedException.class)
     public ErrorMessage handleUnAuthError(UnauthorizedException e) {
-        return new ErrorMessage(e.getLocalizedMessage(), e.getClass().getSimpleName());
+        return new ErrorMessage(e.getMessage(), e.getClass().getSimpleName());
     }
 }
